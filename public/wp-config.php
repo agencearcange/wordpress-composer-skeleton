64ff00878da4d0693b0c68448bf957b32420a525
Thomas Ferney <thomas.ferney@agencearcange.fr>
2016-10-31 09:06:42 +0100

    Clean wp-config

--- public/wp-config.php
+++ public/wp-config.php
@@ -68,18 +68,6 @@ if (WORDPRESS_ENV === 'DEV') {
     /** MySQL hostname */
     define('DB_HOST', 'localhost');
 
-    /**
-     * For developers: WordPress debugging mode.
-     *
-     * Change this to true to enable the display of notices during development.
-     * It is strongly recommended that plugin and theme developers use WP_DEBUG
-     * in their development environments.
-     *
-     * For information on other constants that can be used for debugging,
-     * visit the Codex.
-     *
-     * @link https://codex.wordpress.org/Debugging_in_WordPress
-     */
     define('WP_DEBUG', FALSE); 
     // Enable direct method for update
     define('FS_METHOD','direct');
